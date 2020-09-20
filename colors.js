305152d18d75b32b3ec377bbd768c944ff42d5fb
Seungwon <csw213@naver.com>
2020-09-20 18:26:21 +0900

    study abt Javascript
    study abt Javascript

---
+++ colors.js
@@ -0,0 +1,34 @@
+var Links = {
+  setColor: function(color) {
+    var aList = document.querySelectorAll('a');
+    var i = 0;
+    while (i < aList.length) {
+      aList[i].style.color = color;
+      i = i + 1;
+    }
+  }
+}
+var Body = {
+  setColor: function(color) {
+    document.querySelector('body').style.color = color;
+    //$('body').css('color', color);
+  },
+  setBackgroundColor: function(color) {
+    document.querySelector('body').style.backgroundColor = color;
+    //$('body').css('backgroundColor', color);
+  }
+}
+
+function nightDayHandler(self) {
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+    Links.setColor('blue');
+  }
+}
